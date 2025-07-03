7cf2efcc45fd5825faf64652d0b8add9e8390271
Vedant3008 <shahh7107@gmail.com>
2025-07-03 07:50:40 +0530

    First Code

---
+++ start.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A[5];
+    A[0]=12;
+    A[1]=14;
+    A[2]=16;
+    cout<<sizeof(A)<<endl;
+    cout<<A[1]<<endl;
+    return 0;
+}
